9109c57d2b0a869102cfe09395f6a621bfc24d9f
Cenk Gündoğan <cnkgndgn@gmail.com>
2015-12-09 23:31:51 +0100

    add custom shell command

--- application/main.c
+++ application/main.c
@@ -14,17 +14,43 @@
 #include <stdio.h>
 #include "shell.h"
 #include "msg.h"
+#include <stdbool.h>
+#include <string.h>
+#include "board.h"
 
 #define Q_SZ    (8)
 static msg_t msg_q[Q_SZ];
+static bool led_status = false;
 
 /* Add the shell command function here ###################################### */
+
+int led_control(int argc, char **argv)
+{
+    if (argc == 2) {
+        if (strcmp(argv[1], "on") == 0) {
+            led_status = true;
+            LED_ON;
+            return 0;
+        }
+        else if (strcmp(argv[1], "off") == 0) {
+            led_status = false;
+            LED_OFF;
+            return 0;
+        }
+    }
+
+    printf("Usage: %s [on|off]\n", argv[0]);
+
+    return -1;
+}
 
 /* ########################################################################## */
 
 static const shell_command_t shell_commands[] = {
 
 /* Add a new shell command here ############################################# */
+
+    { "led", "use 'led on' to turn the LED on and 'led off' to turn the LED off", led_control },
 
 /* ########################################################################## */
 
@@ -36,6 +62,8 @@ int main(void)
     msg_init_queue(msg_q, Q_SZ);
 
     puts("Type 'help' for a list of available commands");
+
+    LED_OFF;
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
